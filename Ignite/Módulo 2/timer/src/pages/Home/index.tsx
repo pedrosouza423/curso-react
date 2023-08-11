4dc55a8a53764d292f1e833c7f726ad858753593
Pedro Souza <pedro.souza4237@gmail.com>
2023-08-10 22:39:04 -0300

    Added the content and styled the HISTORY

--- Ignite/Módulo 2/timer/src/pages/Home/index.tsx
+++ Ignite/Módulo 2/timer/src/pages/Home/index.tsx
@@ -11,13 +11,26 @@ export const Home = () => {
             type="text"
             id="task"
             placeholder="Dê um nome para o seu projeto"
+            list="task-sugestion"
           />
+
+          <datalist id="task-sugestion">
+            <option value="Front-End" />
+            <option value="Back-End" />
+            <option value="Full-Stack" />
+            <option value="Mobile" />
+            <option value="Data Science" />
+            <option value="Machine Learning" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
