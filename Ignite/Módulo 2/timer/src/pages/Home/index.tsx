24e07c2d1707161e21977d4784f17a27d08bc0c2
Pedro Souza <pedro.souza4237@gmail.com>
2023-08-13 20:47:36 -0300

    Created the activeCycle

--- Ignite/Módulo 2/timer/src/pages/Home/index.tsx
+++ Ignite/Módulo 2/timer/src/pages/Home/index.tsx
@@ -3,6 +3,8 @@ import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, S
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from 'zod';
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -13,7 +15,15 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+}
+
 export const Home = () => {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -24,12 +34,19 @@ export const Home = () => {
 
   const handleCreateNewCycle = (data: NewCycleFormData) => {
     console.log(data);
+    const newCycle: Cycle = {
+      id: uuidv4(),
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
 
-    reset({
-      task: '',
-      minutesAmount: 0,
-    });
-  };
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(newCycle.id)
+    reset();
+  }
+
+  const activeCycle = cycles.filter(c => c.id === activeCycleId)
+  console.log(activeCycle)
 
   const task = watch('task');
   const isSubmitDisabled = !task;
