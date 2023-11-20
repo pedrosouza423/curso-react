18bdb0def9069e8556d388c83775a187aafcb74b
Pedro Souza <pedro.souza4237@gmail.com>
2023-11-19 17:45:25 -0800

    Added routers

--- Ignite/Módulo 2/ignite-timer/src/App.tsx
+++ Ignite/Módulo 2/ignite-timer/src/App.tsx
@@ -1,16 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button/Button'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button variant="primary" />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
