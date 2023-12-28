f7cd4e608b843bb28e3a18258589c555129947ef
Pedro Souza <pedro.souza4237@gmail.com>
2023-12-28 13:01:39 -0800

    Created the enum ActionTypes and moved the reducer to another file

--- Ignite/Módulo 2/ignite-timer/src/contexts/CyclesContext.tsx
+++ Ignite/Módulo 2/ignite-timer/src/contexts/CyclesContext.tsx
@@ -1,17 +1,9 @@
 import { createContext, useReducer, useState } from 'react'
+import { ActionTypes, Cycle, cyclesReducer } from '../reducers/cycles'
 
 interface CreateCycleData {
   task: string
   minutesAmount: number
-}
-
-interface Cycle {
-  id: string
-  task: string
-  minutesAmount: number
-  startDate: Date
-  interruptedDate?: Date
-  fineshedDate?: Date
 }
 
 interface CycleContextType {
@@ -31,62 +23,13 @@ interface CyclesContextProviderProps {
   children: React.ReactNode
 }
 
-interface CycleState {
-  cycles: Cycle[]
-  activeCycleId: string | null
-}
-
 export const CyclesContextProvider = ({
   children,
 }: CyclesContextProviderProps) => {
-  const [cyclesState, dispatch] = useReducer(
-    (state: CycleState, action: any) => {
-      console.log(state)
-      console.log(action)
-
-      switch (action.type) {
-        case 'ADD_NEW_CYCLE':
-          return {
-            ...state,
-            cycles: [...state.cycles, action.payload],
-            activeCycleId: action.payload.id,
-          }
-        case 'INTERRUPT_CURRENT_CYCLE':
-          return {
-            ...state,
-            cycles: state.cycles.map((cycle) => {
-              if (cycle.id === state.activeCycleId) {
-                return {
-                  ...cycle,
-                  interruptedDate: new Date(),
-                }
-              }
-
-              return cycle
-            }),
-            activeCycleId: null,
-          }
-        case 'MARK_CURRENT_CYCLE_AS_FINISHED':
-          return {
-            ...state,
-            cycles: state.cycles.map((cycle) => {
-              if (cycle.id === state.activeCycleId) {
-                return {
-                  ...cycle,
-                  fineshedDate: new Date(),
-                }
-              }
-
-              return cycle
-            }),
-            activeCycleId: null,
-          }
-        default:
-          return state
-      }
-    },
-    { cycles: [], activeCycleId: null },
-  )
+  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
+    cycles: [],
+    activeCycleId: null,
+  })
 
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
@@ -95,7 +38,7 @@ export const CyclesContextProvider = ({
 
   function markCurrentCycleAsFineshed() {
     dispatch({
-      type: 'MARK_CURRENT_CYCLE_AS_FINISHED',
+      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
       payload: activeCycleId,
     })
   }
@@ -114,7 +57,7 @@ export const CyclesContextProvider = ({
     }
 
     dispatch({
-      type: 'ADD_NEW_CYCLE',
+      type: ActionTypes.ADD_NEW_CYCLE,
       payload: newCycle,
     })
     setAmountSecondsPassed(0)
@@ -122,7 +65,7 @@ export const CyclesContextProvider = ({
 
   function interruptCurrentCycle() {
     dispatch({
-      type: 'INTERRUPT_CURRENT_CYCLE',
+      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
       payload: activeCycleId,
     })
   }
---
+++ Ignite/Módulo 2/ignite-timer/src/reducers/cycles.tsx
@@ -0,0 +1,66 @@
+export interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date
+  interruptedDate?: Date
+  fineshedDate?: Date
+}
+
+interface CycleState {
+  cycles: Cycle[]
+  activeCycleId: string | null
+}
+
+export enum ActionTypes {
+  ADD_NEW_CYCLE = 'ADD_NEW_CYCLE',
+  INTERRUPT_CURRENT_CYCLE = 'INTERRUPT_CURRENT_CYCLE',
+  MARK_CURRENT_CYCLE_AS_FINISHED = 'MARK_CURRENT_CYCLE_AS_FINISHED',
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function cyclesReducer(state: CycleState, action: any) {
+  console.log(state)
+  console.log(action)
+
+  switch (action.type) {
+    case ActionTypes.ADD_NEW_CYCLE:
+      return {
+        ...state,
+        cycles: [...state.cycles, action.payload],
+        activeCycleId: action.payload.id,
+      }
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE:
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return {
+              ...cycle,
+              interruptedDate: new Date(),
+            }
+          }
+
+          return cycle
+        }),
+        activeCycleId: null,
+      }
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return {
+              ...cycle,
+              fineshedDate: new Date(),
+            }
+          }
+
+          return cycle
+        }),
+        activeCycleId: null,
+      }
+    default:
+      return state
+  }
+}
